203efbc99998b7bdb4db383236a97072fa4f7b45
gsach <sper13@gmail.com>
2017-01-16 14:44:44 +0000

    Add test coverage for Oxford input

--- src/test/java/com/develogical/app/QueryProcessorTest.java
+++ src/test/java/com/develogical/app/QueryProcessorTest.java
@@ -23,4 +23,9 @@ public class QueryProcessorTest {
     public void isNotCaseSensitive() throws Exception {
         assertThat(queryProcessor.process("shakespeare"), containsString("playwright"));
     }
+
+    @Test
+    public void knowsAboutOxford() throws Exception {
+        assertThat(queryProcessor.process("oxford"), containsString("South East"));
+    }
 }
