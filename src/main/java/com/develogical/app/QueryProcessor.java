bf4d9edddaffcd657c4eca2a6eb3b4fc6d83f090
George Sachpatzidis <sper13@gmail.com>
2017-01-16 14:20:53 +0000

    Add support for Oxford query

--- src/main/java/com/develogical/app/QueryProcessor.java
+++ src/main/java/com/develogical/app/QueryProcessor.java
@@ -7,6 +7,11 @@ public class QueryProcessor {
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         }
+
+        else if (query.toLowerCase().contains("oxford")) {
+            return "Oxford is a city in the South East region of England and the county town of Oxfordshire. With a population of 159,994";
+        }
+
         return "";
     }
 }
